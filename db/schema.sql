77d63c15ccea7f03a354182cb5436184c34fd73d
Sal Tamay <sal.tamay@outlook.com>
2019-12-14 14:46:45 -0500

    feat: Add schema that creates burger_db and the burgers table

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,15 @@
--- create database and table schema here
+--- Schema
+
+CREATE DATABASE burger_db;
+USE burger_db;
+
+CREATE TABLE burgers
+(
+  id int NOT NULL
+  AUTO_INCREMENT,
+	burger_name varchar
+  (255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
+	PRIMARY KEY
+  (id)
+);
