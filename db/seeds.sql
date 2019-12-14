7a1e30fa29e98a8e52fe4b410abacea41036e40d
Sal Tamay <sal.tamay@outlook.com>
2019-12-14 14:49:07 -0500

    feat: Add seed file that creates dn entries for 3 popular burgers

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,1 +1,12 @@
--- insert burgers here
+INSERT INTO burgers
+  (burger_name)
+VALUES
+  ('True North');
+INSERT INTO burgers
+  (burger_name)
+VALUES
+  ('Hawaiian');
+INSERT INTO burgers
+  (burger_name)
+VALUES
+  ('Nacho');
