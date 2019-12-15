373b308cdeee4514455ed0021ca68b1540f7ef56
Sal Tamay <sal.tamay@outlook.com>
2019-12-15 17:54:29 -0500

    feat: Update schema and seeds file to include new values

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,15 @@
+DROP DATABASE IF EXISTS burger_db;
+
 CREATE DATABASE burger_db;
 USE burger_db;
 
 CREATE TABLE burgers
 (
   id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
+	burger_name VARCHAR(255) NOT NULL,
+	burger_info VARCHAR(255),
 	devoured BOOLEAN DEFAULT false,
+	img_url VARCHAR(255),
+	num_of_likes SMALLINT,
 	PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,12 +1,9 @@
 INSERT INTO burgers
-  (burger_name)
+  (burger_name, burger_info, img_url, num_of_likes)
 VALUES
-  ('True North');
-INSERT INTO burgers
-  (burger_name)
-VALUES
-  ('Hawaiian');
-INSERT INTO burgers
-  (burger_name)
-VALUES
-  ('Nacho');
+  ('True North', 'Aged Cheddar, Bacon, Signature Sauce, Maple Infused Onions.', './assets/images/the-true-north.png', 367),
+  ('Hawaiian', 'Ginger Glazed Pineapple, Bacon, Wasabi Mayo, Hot Pepper Relish, Lettuce.', './assets/images/hawaiian.png', 238),
+  ('Nacho', 'Aged Cheddar, Lettuce, Fresh Tomato Salsa, Jalapeno Sour Cream, Pepper Jack, Guacamole.', './assets/images/nacho.png', 167),
+  ('Mushroom & Swiss', 'Sautéed Mushrooms, Swiss Cheese, Tomatoes, Garlic Mayo, Lettuce.', './assets/images/swiss.png', 198),
+  ('BBQ Bacon', '40 Creek Whisky BBQ, Bacon, Guacamole, Aged Cheddar, Jalapeños', './assets/images/bbq-bacon.png', 398),
+  ('Spicy Piri-Piri', 'Spicy Piri Piri, Bacon, Pepper Jack, Cajun Onions, Tomatoes, Signature Sauce, Lettuce', './assets/images/piri-piri.png', 127);
