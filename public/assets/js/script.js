819a7fc10a400a166c818194b96f38dd100f511c
Sal Tamay <sal.tamay@outlook.com>
2019-12-16 12:04:38 -0500

    bug: Buttons work when clicked and logs the burger id

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,1 +1,10 @@
 // jquery code goes here
+// document.querySelector('.btn').addEventListener('click', e => {
+//   e.preventDefault();
+
+//   console.log('OK');
+// })
+
+function handleClick() {
+  console.log(this.dataset.id);
+}
