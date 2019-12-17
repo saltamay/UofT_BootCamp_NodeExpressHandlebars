08fd8d38525e9a29000784e0a685c4d0d21ae897
Sal Tamay <sal.tamay@outlook.com>
2019-12-17 01:43:19 -0500

    feat: User can add byop custom burger

--- controllers/burgersController.js
+++ controllers/burgersController.js
@@ -55,16 +55,43 @@ router.get('/byob', (req, res) => {
     secondRow: ['Green Olives', 'Dijon Mustard', 'Honey Mustard', 'Yellow Mustard', 'Ketchup', 'BBQ Sauce', 'Hot Sauce', 'Jalapeno', 'Sour Cream'],  
     thirdRow: ['Signature Sauce', 'Gourmet Dill Pickle Relish', 'Hot Pepper Relish', 'Mango Chutney Relish', 'Sweet Relish', 'Garlic Mayo', 'Wasabi Mayo']
   }
+
+  const onTheGrill = [
+    {
+      img_url: './assets/images/byob-burger.png',
+      burger_info: 'Our beef is 100% fresh, never frozen, Canadian Angus. Raised without the use of antibiotics or hormones. No fillers, no steroids.'
+    },
+    {
+      img_url: './assets/images/byob-veggie.png',
+      burger_info: 'Soy based. Low in fat, high in fibre. Vegan friendly.'
+    },
+    {
+      img_url: './assets/images/byob-chicken.png',
+      burger_info: 'Whole chicken breast, no additives, hormone free, grain fed, flame grilled.'
+    },
+    {
+      img_url: './assets/images/byob-hotdog.png',
+      burger_info: 'Grilled, 100% beef. No added hormones, antibiotics, or by-products.'
+    },
+    {
+      img_url: './assets/images/byob-turkey.png',
+      burger_info: 'Grain-fed turkey, grilled fresh to order. No added hormones or steroids.'
+    }
+  ]
   res.render('byob', {
     premiums: premiumToppings,
-    toppings: toppings
+    toppings: toppings,
+    grills: onTheGrill
   });
 })
 
 router.post('/api/burgers', async (req, res) => {
   try {
-    console.log(req.body.name);
-    const result = await createBurger(["burger_name"], [req.body.name]);
+    const { burger_name, burger_info, devoured, img_url } = req.body;
+    const result = await 
+    createBurger(
+      ["burger_name", "burger_info", "devoured", "img_url"], 
+      [burger_name, burger_info, devoured, img_url]);
     if (result.affectedRows === 0) {
       // If no rows were affected, then the neww burger could not been added, so 404
       return res.status(404).end();
